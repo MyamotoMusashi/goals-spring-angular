13b11966645ca04275e4e04a9acaa70cdf1f8bd1
Georgi Dragnev <georgi.dragnev@tek-experts.com>
2018-08-09 23:29:36 +0300

    injected task.service in task.component

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  task = {};
 
-  constructor() { }
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+	const requestID = this.route.snapshot.paramMap.get('requestID');
+	const taskID = this.route.snapshot.paramMap.get('taskID');
+	console.log(requestID);
+	console.log(taskID);
+	console.log(this.taskService);
+	this.taskService.getTaskByID(requestID,taskID).subscribe(task => {
+		this.task = task;
+	})
   }
-
 }
