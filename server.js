871d21d1b698a6184c6a12d02f87731283efd7fa
Miyamoto Musashi <knifed313@gmail.com>
2018-08-11 20:40:34 +0300

    formatted server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // server.js
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const db			 = require('./server/config/db');
-const path			 = require("path"); 
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./server/config/db');
+const path = require("path");
 
-const app            = express();
+const app = express();
 
 const port = 8000;
 
@@ -16,9 +16,6 @@ app.set('view engine', 'pug');
 app.use('/libs', express.static(path.join(__dirname, 'node_modules')));
 app.use('/', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/tests', express.static(path.join(__dirname, 'tests')));
-
-
-
 
 MongoClient.connect(db.url, (err, database) => {
   // Make sure you add the database name and not the collection name
