c807d851dcd7046d408f35cdbad23c7c177fe314
Miyamoto Musashi <knifed313@gmail.com>
2018-08-11 20:55:43 +0300

    request.service.ts now uses HttpClient

--- src/app/request.service.ts
+++ src/app/request.service.ts
@@ -1,21 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getRequestByID(id: string) {
     return this.http.get(`/api/requests/${id}`)
-      .pipe(map((response: any) => response.json()));
   }
 
   deleteRequestByID(id: string) {
     return this.http.delete(`/api/requests/${id}`)
-      .pipe(map((response: any) => response.json()));
   }
 }
